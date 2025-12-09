fdb6f97e27cfe4fcaaeb4f391b919453adf3b9a5
Greg Land <landjgregory@gmail.com>
2025-12-09 00:49:33 -0500

    Might as well pit it on github
    Working.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Intentionally Left Blank
+This is a dumb little program I use to prevent burn-in on my monitors while
+using tiling window managers.
+
+Shout-out and special thanks to the anonymous saint who has run
+https://this-page-intentionally-left-blank.org/ for years.
+
+## License
+SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of either:
+
+ * the GNU General Public License version 2 only, or
+ * the GNU General Public License version 3 only,
+
+as published by the Free Software Foundation.
+
---
+++ src/intentionally_left_blank/__main__.py
@@ -0,0 +1,24 @@
+import sys
+from PySide6.QtCore import Qt
+from PySide6 import QtGui
+from PySide6 import QtWidgets
+from typing import Final
+
+DEFAULT_TOOL_TIP_TEXT: Final[str] = "Intentionally Left Blank"
+DEFAULT_APP_NAME: Final[str] = "intentionally-left-blank"
+DEFAULT_OPACITY: Final[float] = 0.5
+
+def main() -> int:
+    app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName(DEFAULT_APP_NAME)
+    app.setPalette(QtGui.QColor.fromRgb(0,0,0))
+
+    mainWindow = QtWidgets.QMainWindow()
+    mainWindow.setToolTip(DEFAULT_TOOL_TIP_TEXT)
+    mainWindow.setWindowOpacity(DEFAULT_OPACITY)
+    mainWindow.show()
+
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
